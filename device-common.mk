42a4d0367f5383e10f771a50b2b4ca7d67537119
Alessandro Astone <ales.astone@gmail.com>
2020-02-26 15:09:19 +0100

    universal9830: Build and enable fastbootd
    Change-Id: I0b20600fe7203a7aec19cbea8f6849052585c6ab
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -70,6 +70,10 @@ PRODUCT_PACKAGES += \
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl:64 \
