6d3ccd59eb8c005feaf4174e6c1ee715df8d351f
Tim Zimmermann <tim@linux4.de>
2020-04-21 10:11:09 +0200

    universal9830: add wireless powershare
    Change-Id: I815fbf72c719ae98e41e5760f72f09f3a818abdc
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -228,6 +228,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/power/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# PowerShare
+PRODUCT_PACKAGES += \
+    vendor.lineage.powershare@1.0-service.samsung
+
 # Recovery
 PRODUCT_PACKAGES += \
     init.recovery.exynos990.rc
