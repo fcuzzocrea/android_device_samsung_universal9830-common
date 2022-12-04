222cd7e1f8a6e64beed2c38059fef73694ba2dc9
Simon1511 <simon2002.schoenmackers@gmail.com>
2022-12-04 15:30:28 +0100

    universal9830-common: Import touch HAL from hardware/samsung and add singletap support
    Change-Id: I667f7a260e2ef1db24cbaa804897cf28a0cec9da

--- device-common.mk
+++ device-common.mk
@@ -354,7 +354,7 @@ PRODUCT_PACKAGES += \
 
 # Touch features
 PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.samsung
+    vendor.lineage.touch@1.0-service.universal9830
 
 # TUI
 PRODUCT_PACKAGES += \
