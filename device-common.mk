e9c2f5cbbc25a31f0a984868f7ee182473c0f9a3
Tim Zimmermann <tim@linux4.de>
2023-04-06 16:49:39 +0200

    universal9830: Add libutils-v32 shim
    * For sensors.sensorhub.so
    * Abort message: 'incStrongRequireStrong() called on 0x7586a3a1c0 which isn't already owned'
    
    Change-Id: I792d3e1861f7ee721081997eeacc29c27aad3eef
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -337,6 +337,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.samsung
 
+# VNDK
+PRODUCT_PACKAGES += \
+    libutils-v32
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
