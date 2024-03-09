4d746ed9ba7e4556d1a1c284fa60258df728f912
Tim Zimmermann <tim@linux4.de>
2024-03-09 13:34:31 +0100

    universal9830: Convert libaudioproxy to soong
    Change-Id: I0f92df83a4d6cc042ce2aad04331b0b06669984d
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,13 +34,14 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 BOARD_LOW_LATENCY_CAPTURE_DURATION := 20
 BOARD_USB_PLAYBACK_DURATION := 5
 BOARD_USE_MMAP_HW_VOLUME_CONTROL := true
-BOARD_USE_SEC_AUDIO_DYNAMIC_NREC := true
-BOARD_USE_SEC_AUDIO_PARAM_UPDATE := false
-BOARD_USE_SEC_AUDIO_RESAMPLER := true
-BOARD_USE_SEC_AUDIO_SAMSUNGRECORD := true
-BOARD_USE_SEC_AUDIO_SOUND_TRIGGER_ENABLED := true
-BOARD_USE_SEC_AUDIO_SUPPORT_LISTENBACK_DSPEFFECT := true
-BOARD_USE_USB_OFFLOAD := true
+$(call soong_config_set,universal9830AudioVars,use_sec_audio_dynamic_nrec,true)
+$(call soong_config_set,universal9830AudioVars,use_sec_audio_param_update,false)
+$(call soong_config_set,universal9830AudioVars,use_sec_audio_resampler,true)
+$(call soong_config_set,universal9830AudioVars,use_sec_audio_samsungrecord,true)
+$(call soong_config_set,universal9830AudioVars,use_sec_audio_sound_trigger_enabled,true)
+$(call soong_config_set,universal9830AudioVars,use_sec_audio_support_listenback_dspeffect,true)
+$(call soong_config_set,universal9830AudioVars,use_soundtrigger_hal,true)
+$(call soong_config_set,universal9830AudioVars,use_usb_offload,true)
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
