0070b999779b1806e7356963d94242691a68d8a8
LuK1337 <priv.luk@gmail.com>
2024-03-18 18:18:12 +0100

    universal9830: Use libcrypto-v33 from hardware/lineage/compat
    Because vndk prebuilt has SONAME set to libcrypto.so, it prevents linker
    from loading libcrypto.so built from source.
    
    Change-Id: Ic9e29f7b00b469163f48ea407fc9b5d5e90816c8
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -353,10 +353,8 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_PACKAGES += \
+    libcrypto-v33 \
     libutils-v32
-
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libcrypto.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcrypto-v33.so
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
