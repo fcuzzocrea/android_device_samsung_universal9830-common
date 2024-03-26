4db0bdddc374199ee56dc8df6dc8495427ba7973
Tim Zimmermann <tim@linux4.de>
2024-03-26 12:31:21 +0800

    universal9830: Rename libcrypto-v33 to libcrypto-tm
    * To avoid conflicting with hardware/lineage/compat one...

--- device-common.mk
+++ device-common.mk
@@ -137,9 +137,9 @@ PRODUCT_PACKAGES += \
     libkeymaster4_1support.vendor:64
 
 PRODUCT_PACKAGES += \
-    libcrypto-v33 \
+    libcrypto-tm \
     libcrypto_shim:64 \
-    libssl-v33
+    libssl-tm
 
 # Lineage Health
 PRODUCT_PACKAGES += \
